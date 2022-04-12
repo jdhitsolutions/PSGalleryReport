6977dd584e8e61d71f270b4549709509722c89ac
jdhitsolutions <jhicks@jdhitsolutions.com>
2022-04-12 09:42:29 -0400

    updated tag list

--- scripts/make-taglist.ps1
+++ scripts/make-taglist.ps1
@@ -5,31 +5,42 @@ $tmpData = "$env:temp\psgallery.xml"
 
 $out = "C:\scripts\PSGalleryReports\taglist.txt"
 
-Set-Content -Value "This is a list of unique tags found in the PowerShell Gallery. Updated $(Get-Date -Format U) UTC" -Path $out -Encoding utf8
+$open = @"
+This is a list of approximately 6500 unique tags found in the PowerShell Gallery. However, do not assume this is an authoritative or definitive list. Best efforts have been made to parse tags but tag definitions are not consistent across all published modules.
+
+Updated: $(Get-Date -Format U) UTC
+"@
+$splat = @{
+    Value    = $open
+    Path     = $out
+    Encoding = "utf8"
+}
+Set-Content @splat
 
 $list = [System.Collections.Generic.list[string]]::new()
 if (Test-Path $tmpData) {
     #newest is the default
     Write-Host "[$(Get-Date)] Creating tag list" -ForegroundColor magenta
-    $modules = (Import-Clixml $env:temp\psgallery.xml).Where({$_.additionalmetadata.tags})
+    $modules = (Import-Clixml $env:temp\psgallery.xml).Where({ $_.additionalmetadata.tags })
     Write-Host "[$(Get-Date)] Found $($modules.count) modules with defined tags" -ForegroundColor magenta
-    foreach($mod in $modules) {
-        if ($mod.additionalmetadata.tags -contains ",") {
-            #convert all tags to lower case
-           $tags = $mod.additionalmetadata.tags.split(",").Foreach({$_.tolower()})
+    foreach ($mod in $modules) {
+        #convert all tags to lower case
+        if ($mod.additionalmetadata.tags -match ",") {
+            $tags = $mod.additionalmetadata.tags.split(",").Foreach({ $_.tolower() })
         }
         else {
-            $tags = $mod.additionalmetadata.tags.split().Foreach({$_.tolower()})
+            $tags = $mod.additionalmetadata.tags.split().Foreach({ $_.tolower() })
         }
         foreach ($tag in $tags) {
-            If (-Not $list.Contains($tag)) {
-                $list.add($tag)
+            If ( (-Not $list.Contains($tag.trim())) -AND ($tag -ne '-')) {
+                $list.add($tag.Trim())
             }
         }
     } #foreach module
 
     Write-Host "[$(Get-Date)] Found $($list.count) unique tags" -ForegroundColor magenta
     #Sort the list
+    Write-Host "[$(Get-Date)] Saving sorted list to $out" -ForegroundColor magenta
     $list | Sort-Object | Add-Content -Path $out -Encoding utf8
 }
 else {
@@ -41,6 +52,10 @@ Write-Host "[$(Get-Date)] Ending $($myinvocation.mycommand)" -ForegroundColor ma
 <#
 Change Log
 
+4/12/2022
+  Revised opening paragraph.
+  Corrected bug that was improperly splitting comma-separated tags.
+  Added code to trim spaces around tags.
 4/11/2022
   Initial version
 #>
